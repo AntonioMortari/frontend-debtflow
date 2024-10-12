96bfcad2749eee6390a2ad8834ec6d4e7ffce05c
Antonio Mortari <antoniomortari09@gmail.com>
2024-10-12 09:31:13 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: LoginComponent},
+    {path: 'sign-up', component: SignUpComponent},
+    {path: 'dashboard', component: DashboardComponent}
+];
